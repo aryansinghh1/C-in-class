b6cb57ab9385bf5d614dbda1da5197382f9fe20c
aryansinghh1 <aryansinghkush2522@gmail.com>
2024-09-11 10:41:38 +0530

    insertion at last

---
+++ c/linked list insertion at last.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void insert_b(struct node **head_ref, int element)
+{
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data=element;
+    newnode->next=*head_ref;
+    *head_ref=newnode;
+}
+
+void print_list(struct node *temp)
+{
+    while (temp != NULL)
+    {
+        printf("%d",temp->data);
+        temp = temp->next;
+    }
+}
+int main()
+{
+    struct node *head = NULL;
+    int n, element;
+
+    scanf("%d",&n);
+    for(int i = 1; i<=n;i++)
+    {
+        scanf("%d",&element);
+        insert_b(&head,element);
+
+    }
+    print_list(head);
+    
+    return 0;
+
+}
